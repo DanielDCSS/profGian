be37067e2da14bac57cdc1b52e9e2a2d9c4a189d
DanielDCSS <danielcarlosss12@icloud.com>
2024-07-16 17:47:52 -0300

    aqui vao os ajustes com os botoes consertados

--- src/App.jsx
+++ src/App.jsx
@@ -18,36 +18,6 @@ import data from './Textos.json'
 
 function App() {
 
-  /* 
-   window.onload = function() {
-                setTimeout(function() {
-                    document.querySelector('.info').style.opacity = '1';
-                    document.querySelector('.info').style.transform = 'translateY(0)';
-                }, 400);
-                setTimeout(function() {
-                    document.querySelector('.content').style.opacity = '1';
-                    document.querySelector('.content').style.transform = 'translateY(0)';
-                }, 600);
-                setTimeout(function() {
-                    document.querySelector('.publics').style.opacity = '1';
-                    document.querySelector('.publics').style.transform = 'translateY(0)';
-                }, 800);
-                setTimeout(function() {
-                    document.querySelector('.contato').style.opacity = '1';
-                    document.querySelector('.contato').style.transform = 'translateY(0)';
-                }, 1000);
-                setTimeout(function() {
-                    document.querySelector('.foto').style.opacity = '1';
-                    document.querySelector('.foto').style.transform = 'translateY(0)';
-                }, 1200);
-                setTimeout(function() {
-                    document.querySelector('.logo').style.opacity = '1';
-                    document.querySelector('.logo').style.transform = 'translateY(0)';
-                }, 1000);
-            }; 
-            Implementar essa função no JSX
-            Dom tree => Hooks do React
-  */
 
   var txt_abt_me = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.Lorem ipsum dolor sit amet,'
 
@@ -76,12 +46,12 @@ function App() {
 
         <ContentListing secName={'pesquisa'} titulo='Pesquisa' img_size={'250px'} UrlImg="./src/assets/pesquisa.jpg" img_class={'float-image-right'} texto={pesq}  subtitle={'Tópicos de interesse'} itens={items}/>
 
-        <ContentListing secName={'pesquisa'} titulo='Publicacões' img_size={'250px'} UrlImg="./src/assets/publicacoes.png" img_class={'float-image-left'} texto={publi}  subtitle={'Publicações de destaque'} itens={items}/>
+        <ContentListing secName={'publicacões'} titulo='Publicacões' img_size={'250px'} UrlImg="./src/assets/publicacoes.png" img_class={'float-image-left'} texto={publi}  subtitle={'Publicações de destaque'} itens={items}/>
 
 
-        <ContentListing secName={'pesquisa'} titulo='Ensino' img_size={'250px'} UrlImg="./src/assets/ensino.jpg" img_class={'float-image-right'} texto={ens}  subtitle={'Matérias lecionadas'} itens={items}/>
+        <ContentListing secName={'ensino'} titulo='Ensino' img_size={'250px'} UrlImg="./src/assets/ensino.jpg" img_class={'float-image-right'} texto={ens}  subtitle={'Matérias lecionadas'} itens={items}/>
 
-        <Contato0  id ="contact" alt="" titulo="Contato"/>
+        <Contato0 secName={'contato'} id ="contact" alt="" titulo="Contato"/>
 
         <Footer0 />
       
--- src/Head0.jsx
+++ src/Head0.jsx
@@ -1,57 +1,65 @@
-import './index.css'
+import React, { useEffect, useState } from 'react';
+import './index.css';
 
-function Head0(){
+function Head0() {
     const UrlHead = "./src/assets/logoIfusp.png";
-    /*
-    document.addEventListener('DOMContentLoaded', function() {
-        window.scrollToSection = function(sectionId) {
-            const targetSection = document.getElementById(sectionId);
-            window.scrollTo({
-                top: targetSection.offsetTop,
-                behavior: 'smooth'
-            });
-        };
-    
-        window.addEventListener('scroll', function() {
-            var header = document.querySelector('header');
-            if (window.scrollY > 0) {
-                header.classList.add('scrolled');
-            } else {
-                header.classList.remove('scrolled');
-            }
+
+    const scrollToSection = (sectionId) => {
+        const targetSection = document.getElementById(sectionId);
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth',
         });
-    });  Para fazer isso temos que usar os hooks do React*/
+    };
+
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 0) {
+                setScrolled(true);
+            } else {
+                setScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
         <div>
             <div className="cabecalho">
-                <header className="Header">
+                <header className={`Header ${scrolled ? 'scrolled' : ''}`}>
                     <div className="container">
                         <div className="apresentacao">
                             <div className="logo">
-                                <img style={{width: '50px'}} src={UrlHead}/>
+                                <img style={{ width: '50px' }} src={UrlHead} alt="Logo" />
                             </div>
                             <div className="name-university">
                                 <h2>Giancarlo Espósito</h2>
                                 <p>Instituto de física da USP</p>
                             </div>
                         </div>
-                
-                
+
                         <div className="botoes-pagina">
                             <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
-                            <button onClick={() => scrollToSection('about')} type="button" className="btn btn-outline">Sobre mim</button>
-                                <button onClick={() => scrollToSection('research')} type="button" className="btn btn-outline">Pesquisa</button>
-                                <button onClick={() => scrollToSection('publications')} type="button" className="btn btn-outline">Publicações</button>
-                                <button onClick={() => scrollToSection('teach')} type="button" className="btn btn-outline">Ensino</button>
-                                <button onClick={() => scrollToSection('contact')} type="button" className="btn btn-outline">Contato</button>
+                                <button onClick={() => scrollToSection('sobreMim')} type="button" className="btn btn-outline">Sobre mim</button>
+                                <button onClick={() => scrollToSection('pesquisa')} type="button" className="btn btn-outline">Pesquisa</button>
+                                <button onClick={() => scrollToSection('publicacões')} type="button" className="btn btn-outline">Publicações</button>
+                                <button onClick={() => scrollToSection('ensino')} type="button" className="btn btn-outline">Ensino</button>
+                                <button onClick={() => scrollToSection('contato')} type="button" className="btn btn-outline">Contato</button>
                             </div>
                         </div>
-                
+
                     </div>
                 </header>
             </div>
         </div>
-    )
+    );
 }
+
 export default Head0;
