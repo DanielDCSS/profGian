46d22d7cdd0f6a85a6f117e04687c59fad3a9c93
DanielDCSS <danielcarlosss12@icloud.com>
2024-07-31 16:38:30 -0300

    Deploy 3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
   plugins: [react()],
   base: "/profGian",
+  publicDir: 'public',
+  build: {
+    outDir: 'dist', // Definindo o diretório de saída para dist
+    assetsDir: "assets", // Definindo o diretório de ativos para assets
+  },
 })
