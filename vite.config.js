15983f61237fdf11800bfcf23bbbda05ad8b7160
DanielDCSS <danielcarlosss12@icloud.com>
2024-08-01 10:26:08 -0300

    colocar o site no ar

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: "/profGian",
+  base: "/profGian",
 
   publicDir: 'public',
   build: {
